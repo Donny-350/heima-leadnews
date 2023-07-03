bfe64c854dc9782dbf396485dbbfa83d171a2c90
zilongw <zilongwang1998@163.com>
2023-07-03 13:49:29 +0800

    阿里云-文章审核

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmNewsAutoScanService.java
@@ -0,0 +1,10 @@
+package com.heima.wemedia.service;
+
+public interface WmNewsAutoScanService {
+
+    /**
+     * 自媒体文章审稿
+     * @param id 自媒体文章id
+     */
+    public void autoScanWmNews(Integer id);
+}
