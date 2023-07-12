274b8b2089133f5a957da6fbc4267e9d248d7215
zilongw <zilongwang1998@163.com>
2023-07-12 22:02:12 +0800

    延迟任务服务实现

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
@@ -6,6 +6,7 @@ import com.heima.article.mapper.ApArticleConfigMapper;
 import com.heima.article.mapper.ApArticleContentMapper;
 import com.heima.article.mapper.ApArticleMapper;
 import com.heima.article.service.ApArticleService;
+import com.heima.article.service.ArticleFreemarkerService;
 import com.heima.common.constants.ArticleConstants;
 import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
@@ -37,6 +38,9 @@ public class ApArticleServiceImpl extends ServiceImpl<ApArticleMapper, ApArticle
 
     @Autowired
     private ApArticleContentMapper apArticleContentMapper;
+
+    @Autowired
+    private ArticleFreemarkerService articleFreemarkerService;
 
     private final static short MAX_PAGE_SIZE = 50;
 
@@ -131,6 +135,9 @@ public class ApArticleServiceImpl extends ServiceImpl<ApArticleMapper, ApArticle
             apArticleContent.setContent(dto.getContent());
             apArticleContentMapper.updateById(apArticleContent);
         }
+
+        //异步调用 生成静态文件上传到minio中
+        articleFreemarkerService.buildArticleToMinIo(apArticle, dto.getContent());
 
         //3. 结果返回 文章的id
         return ResponseResult.okResult(apArticle.getId());
---
+++ heima-leadnews-service/heima-leadnews-schedule/src/test/java/com/heima/schedule/test/RedisTest.java
@@ -0,0 +1,34 @@
+package com.heima.schedule.test;
+
+import com.heima.common.redis.CacheService;
+import com.heima.schedule.ScheduleApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = ScheduleApplication.class)
+@RunWith(SpringRunner.class)
+public class RedisTest {
+
+    @Autowired
+    private CacheService cacheService;
+
+    @Test
+    public void testList(){
+
+        //在list的左边添加元素
+        //cacheService.lLeftPush("list_001", "hello,redis");
+        String s = cacheService.lRightPop(("list_001"));
+        System.out.println(s);
+    }
+
+    @Test
+    public void testZset() {
+        //添加数据到zset中 分值
+        cacheService.zAdd("zset_key_001", "hello zset 001", 1000);
+        cacheService.zAdd("zset_key_001", "hello zset 002", 2000);
+        cacheService.zAdd("zset_key_001", "hello zset 003", 3000);
+    }
+}
